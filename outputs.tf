d100c4fe3b8a11f190fdce8a653b9ed3ff1605c0
ARIES5533 <olumokomoses94@gmail.com>
2025-10-20 02:10:13 +0100

    jenkins-update

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "ec2_public_ip" {
   description = "Public IP of EC2 instance"
-  value       = aws_instance.web.public_ip
+  value       = aws_instance.web-jenkins.public_ip
 }
 
 output "ec2_public_dns" {
   description = "Public DNS of EC2 instance"
-  value       = aws_instance.web.public_dns
+  value       = aws_instance.web-jenkins.public_dns
 }
